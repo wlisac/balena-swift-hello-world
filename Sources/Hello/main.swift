4d1ff40b9f559a6c1a201f5813c8b1780d3f5655
Will Lisac <will@lisac.org>
2019-05-19 19:35:02 -0700

    Add example shutdown handlers for SIGINT and SIGTERM

--- Sources/Hello/main.swift
+++ Sources/Hello/main.swift
@@ -1,13 +1,32 @@
+import Dispatch
 import Foundation
 
 print("Hello from Swift 👋")
+
+// Setup shutdown handlers to handle SIGINT and SIGTERM
+// https://www.balena.io/docs/reference/base-images/base-images/#how-the-images-work-at-runtime
+let signalQueue = DispatchQueue(label: "shutdown")
+
+func makeSignalSource(_ code: Int32) {
+    let source = DispatchSource.makeSignalSource(signal: code, queue: signalQueue)
+    source.setEventHandler {
+        source.cancel()
+        print()
+        print("Goodbye")
+        exit(0)
+    }
+    source.resume()
+    signal(code, SIG_IGN)
+}
+
+makeSignalSource(SIGTERM)
+makeSignalSource(SIGINT)
 
 // If the application is running on balena,
 // start the main run loop so that the application doesn't quit.
 //
 // Otherwise the container will be repeatedly restarted by balena.
 // https://www.balena.io/docs/learn/develop/runtime
-//
 if ProcessInfo.processInfo.environment["BALENA"] == "1" {
     RunLoop.main.run()
 }
